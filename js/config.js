c4670a925ec1d442ac03d38faf3d24873d00e6e8
talasago <lyricaldragons@gmail.com>
2021-05-08 17:45:38 +0900

    cognito反映

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: ' ap-northeast-1_nAGOXFXo9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '67onua6jhp41v0pdt2k2p1roi5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
